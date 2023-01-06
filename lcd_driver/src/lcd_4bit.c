40f8832be62713d115a0ce5d7f10805957705e2f
gulsahseher <70262153+gulsahseher@users.noreply.github.com>
2023-01-07 01:31:53 +0300

    Add files via upload

---
+++ lcd_driver/src/lcd_4bit.c
@@ -0,0 +1,62 @@
+#include "msp430.h"
+#include "in430.h"
+#include "lcd_4bit.h"
+
+void lcd_write(unsigned char data)
+{
+  __delay_cycles(40);
+  LCD_DATA = ( ( data >> 4 ) & 0x0F );
+  LCD_STROBE();
+  LCD_DATA = ( data & 0x0F );
+  LCD_STROBE();
+}
+
+void lcd_clear(void)
+{
+  P2OUT&=~BIT6; //LCD_RS = 0;
+  lcd_write(0x01);
+  __delay_cycles(2000);
+}
+
+void lcd_puts(const char * s)
+{
+  P2OUT|=BIT6; //LCD_RS = 1;
+  while(*s)
+  lcd_write(*s++);
+}
+
+void lcd_putch(char c)
+{
+  P2OUT|=BIT6; //LCD_RS = 1;
+  lcd_write(c);
+}
+
+void lcd_goto(unsigned char row, unsigned char column)
+{
+  P2OUT&=~BIT6;
+  if(row==1)
+    lcd_write(0x80+((column-1)%16));
+  else if (row==2)
+    lcd_write(0xC0+((column-1)%16));
+}
+
+
+void lcd_init()
+{
+  P2OUT&=~BIT6; //LCD_RS = 0;
+  P2OUT&=~BIT7; //LCD_EN = 0;
+  __delay_cycles(15000);
+  LCD_DATA = 0x03;
+  LCD_STROBE();
+  __delay_cycles(5000);
+  LCD_STROBE();
+  __delay_cycles(200);
+  LCD_STROBE();
+  __delay_cycles(200);
+  LCD_DATA = 2;
+  LCD_STROBE();
+  lcd_write(0x28);
+  lcd_write(0x0C);
+  lcd_clear();
+  lcd_write(0x06);
+}
